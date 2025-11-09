e426997cf2a03f9a83e50b37a4dd440e52421aa1
officialcbk <eeze@academic.rrc.ca>
2025-11-09 07:30:48 -0600

    app.ts done

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import express, { Request, Response } from 'express';
+import morgan from "morgan";
+import loanRoutes from './api/v1/routes/loanRoutes'; 
+import { errorHandler } from './middleware/errorHandler';
+
+const app = express();
+
+app.use(morgan('combined'));
+
+app.use(express.json());
+
+app.use('/api/v1/loans', loanRoutes);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
+export default app;
