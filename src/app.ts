5c98b38c0c54da269ed43f7bd660b86e9deea61c
officialcbk <eeze@academic.rrc.ca>
2025-11-10 10:12:04 -0600

    Firebase configuratoion done

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,10 @@ import express from 'express';
 import morgan from 'morgan';
 import loanRoutes from './api/v1/routes/loanRoutes'; 
 import { errorHandler } from './api/v1/middleware/errorHandler';
-import { loggingMiddleware } from './api/v1/middleware/loggingMiddleware';
-import logger from './api/v1/utils/logger';
-import admin from './config/firebase'; 
+import { loggingMiddleware } from './api/v1/middleware/loggingMiddleware'; 
+import logger from './api/v1/utils/logger'; 
+import admin from './config/firebase';
+
 
 const app = express();
 
@@ -14,22 +15,13 @@ app.use(express.json());
 
 app.use('/api/v1/loans', loanRoutes);
 
-app.use(errorHandler);
-
 app.use(loggingMiddleware);
 
+app.use(errorHandler);
 
-app.get('/error', (req, res, next) => {
-  const err = new Error('Something went wrong!');
-  next(err); 
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
 
 app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
-  const db = admin.firestore(); 
+  const db = admin.firestore();  // Access Firestore to log errors
 
   db.collection('errorLogs').add({
     message: err.message,
@@ -46,6 +38,7 @@ app.use((err: Error, req: express.Request, res: express.Response, next: express.
 
   res.status(500).send('Internal Server Error');
 });
+
 
 const PORT = process.env.PORT || 5000;
 const server = app.listen(PORT, () => {
